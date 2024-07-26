a6132aadde7d22c7f281ac82cf07fbadf897b9e7
Gagemo <44406774+Gagemo@users.noreply.github.com>
2024-07-26 10:32:20 -0400

    g

--- Scripts/Temp.R
+++ Scripts/Temp.R
@@ -15,7 +15,8 @@ cat("\014")
 #
 #########################     Installs Packages   ##############################
 
-list.of.packages <- c("tidyverse", "vegan", "agricolae")
+list.of.packages <- c("tidyverse", "vegan", "agricolae", "tables", "plotrix",
+                      "ggpubr", "rstatix", "multcompView")
 new.packages <- list.of.packages[!(list.of.packages %in% 
                                      installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
@@ -25,14 +26,25 @@ if(length(new.packages)) install.packages(new.packages)
 library(tidyverse)
 library(vegan)
 library(agricolae)
+library(tables)
+library(plotrix)
+library(ggpubr)
+library(rstatix)
+library(multcompView)
 
 ##########################     Read in Data  ###################################
 
 GRASS = read.csv("Data/Flammability Project - Temp.csv")
-weight = read.csv("Data/Flammability Project - Weight.csv")
 
 str(GRASS)
 summary(GRASS)
+
+GRASS$Species = as.character(GRASS$Species)
+GRASS$T1 = as.numeric(GRASS$T1)
+GRASS$T2 = as.numeric(GRASS$T2)
+
+GRASS$Species = factor(GRASS$Species)
+
 
 ##### Filter for max temperature for T1 (Bottom Probe) and T2 (Top Probe) ######
 
@@ -46,60 +58,151 @@ MxT2 <- GRASS %>%
   summarise(MaxTemp = max(T2)) %>%
   arrange(ID, Species)
 
-MxT1 <- full_join(MxT1, weight, by=c("ID", "Species"))
+GRASS <- cbind(MxT1, MxT2)
+GRASS <- select(GRASS, -ID...4, -Species...5)
 
-## Max Temp1 ##
+# Check Assumptions #
+model  <- lm(MaxTemp...3 ~ Species...2, data = GRASS)
 
-boxT1 = 
-  ggplot(MxT1, aes(x = Species, y = MaxTemp, fill = Species)) +
-  geom_boxplot(alpha = 0.8) +
-  geom_jitter(size=3, alpha = 0.5, color="black", width = 0.25) +
+# Create a QQ plot of residuals
+ggqqplot(residuals(model))
+
+# Compute Shapiro-Wilk test of normality
+shapiro_test(residuals(model))
+plot(model, 1)
+
+# Compute Levene's Test
+GRASS %>% levene_test(MaxTemp...3 ~ Species...2)
+
+########################## Tukey Test - Multiple Comparisons ###################
+anova <- 
+  aov(MaxTemp...3 ~ Species...2, data = GRASS) %>% add_significance()
+summary(anova)
+
+tukey <-TukeyHSD(anova) 
+tukey
+
+HSD = HSD.test(anova, trt = c("Species"))
+HSD
+
+tukey.cld <- multcompLetters4(anova, tukey)
+print(tukey.cld)
+
+dt <- GRASS %>% 
+  group_by(Species...2) %>%
+  summarise(w=mean(exp(MaxTemp...3)), 
+            sd = sd(exp(MaxTemp...3)) / sqrt(n())) %>%
+  arrange(desc(w)) %>% 
+  ungroup() 
+
+# extracting the compact letter display and adding to the Tk table
+cld2 <- data.frame(letters = tukey.cld$`Species`$Letters)
+dt$tukey.cld <- cld2$letters
+
+################### Box Plot - Max Growth Heights ######################
+box = 
+  ggplot(GRASS, aes(x = Species...2, y = MaxTemp...3, fill = Species...2)) + 
+  geom_boxplot(size = 0.5, color="black", show.legend = FALSE) +
+  geom_point(shape=16, show.legend = FALSE, size =2)  +
+  geom_text(data = dt, aes(label = tukey.cld, y = 25), size=10, vjust = 0.5) +
   theme_bw() +
-  theme(panel.grid.major = element_blank(),
+  theme(plot.title = element_blank(),
+        axis.title.x = element_blank(),
+        axis.text.x=element_text(size = 15),
+        axis.title.y =  element_text(margin = unit(c(0, 5, 0, 0), "mm"),
+                                     size = 30, face="bold"),
+        axis.text.y = element_text(size=30, face="bold", color = "black"),
+        panel.background = element_rect(color=NA),
+        plot.background = element_rect(color=NA),
+        panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        panel.background = element_blank(),
-        plot.title = element_text(hjust = 0.5),
-        text=element_text(size=16,  family = "Roboto Mono"))+
-  theme_classic() 
-boxT1
+        legend.background = element_rect(color=NA),
+        legend.box.background = element_rect(color=NA),
+        strip.background = element_blank(),
+        legend.title = element_text(color = "black", size = 30, face="bold"),
+        legend.text = element_text(color = "black", size = 30),
+        strip.text = element_text(color = "black", size = 30, face="bold"),
+        text = element_text(family = "sans")) + 
+  scale_x_discrete(labels = function(x) str_wrap(x, width = 10)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  ylab("Max temperature (C)")
+box
 
-ggsave("Figures/Box_MaxTemp1.png", 
+ggsave("Figures/Box_Max_T1.png", 
        width = 10, height = 7)
 
-# Test for Significances#
-anova = aov(MaxTemp ~ Species+Hood_Humidity+Hood_Temp, data = MxT1)
+################################################################################
+################################################################################
+################################ t2 (top probe) ################################
+################################################################################
+################################################################################
+
+# Check Assumptions #
+model  <- lm(MaxTemp...6 ~ Species...2, data = GRASS)
+
+# Create a QQ plot of residuals
+ggqqplot(residuals(model))
+
+# Compute Shapiro-Wilk test of normality
+shapiro_test(residuals(model))
+plot(model, 1)
+
+# Compute Levene's Test
+GRASS %>% levene_test(MaxTemp...6 ~ Species...2)
+
+########################## Tukey Test - Multiple Comparisons ###################
+anova <- 
+  aov(MaxTemp...6 ~ Species...2, data = GRASS) %>% add_significance()
 summary(anova)
-tukey.one.way<-TukeyHSD(anova)
-tukey.one.way
 
-## Max Temp2 ##
+tukey <-TukeyHSD(anova) 
+tukey
 
-boxT2 = 
-  ggplot(MxT2, aes(x = Species, y = MaxTemp, fill = Species)) +
-  geom_boxplot(alpha = 0.8) +
-  geom_jitter(size=3, alpha = 0.5, color="black", width = 0.25) +
+HSD = HSD.test(anova, trt = c("Species"))
+HSD
+
+tukey.cld <- multcompLetters4(anova, tukey)
+print(tukey.cld)
+
+dt <- GRASS %>% 
+  group_by(Species...2) %>%
+  summarise(w=mean(exp(MaxTemp...6)), 
+            sd = sd(exp(MaxTemp...6)) / sqrt(n())) %>%
+  arrange(desc(w)) %>% 
+  ungroup() 
+
+# extracting the compact letter display and adding to the Tk table
+cld2 <- data.frame(letters = tukey.cld$`Species`$Letters)
+dt$tukey.cld <- cld2$letters
+
+################### Box Plot - Max Growth Heights ######################
+box = 
+  ggplot(GRASS, aes(x = Species...2, y = MaxTemp...6, fill = Species...2)) + 
+  geom_boxplot(size = 0.5, color="black", show.legend = FALSE) +
+  geom_point(shape=16, show.legend = FALSE, size =2)  +
+  geom_text(data = dt, aes(label = tukey.cld, y = 25), size=10, vjust = 0.5) +
   theme_bw() +
-  theme(panel.grid.major = element_blank(),
+  theme(plot.title = element_blank(),
+        axis.title.x = element_blank(),
+        axis.text.x=element_text(size = 15),
+        axis.title.y =  element_text(margin = unit(c(0, 5, 0, 0), "mm"),
+                                     size = 30, face="bold"),
+        axis.text.y = element_text(size=30, face="bold", color = "black"),
+        panel.background = element_rect(color=NA),
+        plot.background = element_rect(color=NA),
+        panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        panel.background = element_blank(),
-        plot.title = element_text(hjust = 0.5),
-        text=element_text(size=16,  family = "Roboto Mono"))+
-  theme_classic() 
-boxT2
+        legend.background = element_rect(color=NA),
+        legend.box.background = element_rect(color=NA),
+        strip.background = element_blank(),
+        legend.title = element_text(color = "black", size = 30, face="bold"),
+        legend.text = element_text(color = "black", size = 30),
+        strip.text = element_text(color = "black", size = 30, face="bold"),
+        text = element_text(family = "sans")) + 
+  scale_x_discrete(labels = function(x) str_wrap(x, width = 10)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  ylab("Max temperature (C)")
+box
 
-ggsave("Figures/Box_MaxTemp2.png", 
+ggsave("Figures/Box_Max_T2.png", 
        width = 10, height = 7)
-
-# Test for Significances#
-anova = aov(MaxTemp ~ Species+Humidity, data = MxT2)
-summary(anova)
-tukey.one.way<-TukeyHSD(anova)
-tukey.one.way
-
-
-head(MxT1)
-
-
-sd(MxT1$MaxTemp)/mean(MxT1$MaxTemp)
